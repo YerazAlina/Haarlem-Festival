a821a2c808701e40b1cbc2f0a000652d31a23988
YerazAlina <70819728+YerazAlina@users.noreply.github.com>
2022-02-10 10:27:01 +0100

    fixed routing

--- app/public/SwitchRouter.php
+++ app/public/SwitchRouter.php
@@ -7,16 +7,19 @@ class SwitchRouter
         switch ($uri) {
             case '':
                 //http://localhost/
-                require __DIR__ . '/views/cms/homecms.php';
+                require __DIR__ . '/views/cms/login.php';
                 break;
-            case 'dashboard':
-                require __DIR__ . '/views/cms/dashboard.php';
+            case 'login':
+                require __DIR__ . '/views/cms/login.php';
+                break;
+            case 'logout':
+                require __DIR__ . '/views/cms/logout.php';
                 break;
             case 'homecms':
                 require __DIR__ . '/views/cms/homecms.php';
                 break;
-            case 'login':
-                require __DIR__ . '/views/cms/login.php';
+            case 'dashboard':
+                require __DIR__ . '/views/cms/dashboard.php';
                 break;
             default:
                 echo '404 not found ';
