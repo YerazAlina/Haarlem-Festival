e9deaf813ac723bc709183cbacbc8045fb9f3f35
MhdMokhtar <m.aboudaher91@gmail.com>
2022-02-11 17:30:22 +0100

    nav

---
+++ app/public/views/navbar.php
@@ -0,0 +1,47 @@
+<nav class="navbar navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="../../img/logo.png" height="70" alt="Haarlem Festival" style="margin-top: -5px;" />
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" style="color: white;" href="homecms">Home</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" style="color: white;" href="dashboard">Food</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" style="color: white;" href="updateprogram">Jazz</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" style="color: white;" href="updateprogram">Dance</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" style="color: white;" href="updateprogram">History</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="d-flex align-items-center" style="gap: 20px;">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" style="color: white; text-decoration: none;">ENG<span class="caret"></span></a>
+        <a class="nav-link" href="#">
+            <i class="bi bi-cart3" style="color: white;"></i>
+        </a>
+        <img src="../../img/avatar.png" alt="Avatar" class="avatar" style="vertical-align: middle; width: 40px; height: 40px; border-radius: 50%;">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" style="color: white; text-decoration: none;"> name <?php ?> </a>
+    </div>
+</nav>
