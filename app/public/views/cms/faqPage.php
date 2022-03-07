f8269a4af947ad2537a712f709fdd3d262b67510
imen19f <70758968+imen19f@users.noreply.github.com>
2022-03-07 15:52:09 +0100

    commit on imen

--- app/public/views/cms/faqPage.php
+++ app/public/views/cms/faqPage.php
@@ -1,21 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Zen+Antique&display=swap" rel="stylesheet">
-    <title>HAARLEM FESTIVAL </title>
+<?php require __DIR__ . '/../head.php'; ?>
 
-</head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light home-page" style="background-color: #A42323;">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="homePage">
-                <img src="../../img/logo.png" height="70" alt="Haarlem Festival" style="margin-top: -5px;" />
+    
+<nav class="navbar navbar-expand-lg navbar-light bg-light home-page " style="background-color: #A42323;">
+        <div class="container">
+            <a class="navbar-brand p-2" href="homePage">
+                <img src="../../img/logo-white.png" height="50" alt="Haarlem Festival" style="margin-top: -5px;" />
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -23,12 +15,21 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Program</a>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="programDropDown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Program
+						</a>
+						<ul class="dropdown-menu" aria-labelledby="programDropDown">
+							<li><a class="dropdown-item" href="#">Food</a></li>
+							<li><a class="dropdown-item" href="#">History</a></li>
+							<li><a class="dropdown-item" href="#">Jazz</a></li>
+							<li><a class="dropdown-item" href="#">Dance</a></li>
+						</ul>
+						
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Festival info</a>
@@ -36,87 +37,72 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">FAQ</a>
                     </li>
+                    </li>
 
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">En</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link"><i class="fa-solid fa-user"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link"><i class="fa-solid fa-cart-shopping"></i></a>
+                    </li>
                 </ul>
             </div>
         </div>
 
-        <div class="d-flex align-items-center" style="gap: 20px;">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"
-            style="color: black; text-decoration: none;">ENG<span class="caret"></span></a>
+        
+</nav>
+
+<div class="container">
+    <!-- FAQ -->
+    <h2 class="my-5">Frequently Asked Questions</h2>
+    <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+				<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+					Will there be overnight accomodation?
+				</button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                <strong>Unfortunately, no.</strong> Haarlem Festival does not offer <a href=""></a> occomedation, but we suggest <a href="https://www.airbnb.com/s/Haarlem--Netherlands/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_dates%5B%5D=april&flexible_trip_dates%5B%5D=march&flexible_trip_lengths%5B%5D=weekend_trip&date_picker_type=calendar&query=Haarlem%2C%20Netherlands&place_id=ChIJ--nhYGzvxUcROX6huIBW4Yo&source=structured_search_input_header&search_type=autocomplete_click">Airbnb</a> as a good a reliable tool to find a roof during the festivities.
+            </div>
+            </div>
         </div>
-    </nav>
-    <div id="accordion">
-  <div class="card">
-    <div class="card-header" id="headingOne">
-      <h5 class="mb-0">
-        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        What are the COVID measurements for events in the Netherlands?
-        </button>
-      </h5>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingTwo">
+				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+					Can I have a refund?
+				</button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+                If the refund is claimed atleast <strong>5 days before the first event, yes.</strong> 
+            </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingThree">
+				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseTwo">
+				What are the COVID measurements for events in the Netherlands?
+				</button>
+            </h2>
+            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+				The latest Dutch press conference mandates that clubs, venues and indoor events are force to close between 03:00 and 06:00 CET. This means HAARLEM FESTIVAL is preparing to shift it's events till 02:00. Over the coming days, the HAARLEM FESTIVAL team and the Amsterdam city council are examining the details of the new regulations for each type of event within the total HAARLEM offering.
+				<br>
+				Also, all events need to have a COVID-health-check prior to the event.
+					
+            </div>
+            </div>
+        </div>
+        
     </div>
-
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-      <div class="card-body">
-    The latest Dutch press conference mandates that clubs, venues and indoor events are force to close between 03:00 and 06:00 CET. 
-    This means HAARLEM FESTIVAL is preparing to shift it's events till 02:00.
-    Over the coming days, the HAARLEM FESTIVAL team and the Amsterdam city council are examining the details of the new regulations for each type of event within the total HAARLEM offering.
-    Also, all events need to have a COVID-health-check prior to the event.
-
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingTwo">
-      <h5 class="mb-0">
-        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        What is the minimum age to get in?
-        </button>
-      </h5>
-    </div>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-      <div class="card-body">
-       
-Most festival events have an NO age limit  (but sometimes 18+), these are Dutch governmental rules which say that below the age of 18 you are not allowed to enter a club. 
-Please double check the entry age to be sure you can enter the event.
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingThree">
-      <h5 class="mb-0">
-        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Collapsible Group Item #3
-        </button>
-      </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 
-        3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.
-         Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="col-md-6 col-lg-4 mb-4">
-      <h6 class="mb-3 text-primary"><i class="fas fa-book-open text-primary pe-2"></i> Another
-        question that is longer than usual</h6>
-      <p>
-        Of course! We’re happy to offer a free plan to anyone who wants to try our service.
-        Contact us on 00316382255.
-      </p>
-    </div>
-  </div>
 </div>
-    <footer class="text-center text-white" style="background-color: #EFE0E0;">
-    <div class="container pt-4">
-        <section class="mb-4" style="color: black;">
-            &copy; 2022 Copyright Haarlem Festival
-        </section>
-    </div>
-    <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.06);">
-        <a class="text-dark">Privacy Policy / Terms of Use / Cookie Policy / Access Equality</a>
-    </div>
-</footer>
+
+<?php require __DIR__ . '/footer.php'; ?>
 </body>
-</html>
--- app/public/views/head.php
+++ app/public/views/head.php
@@ -7,4 +7,6 @@
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Zen+Antique&display=swap" rel="stylesheet">
     <title>Document</title>
+    <script src="https://kit.fontawesome.com/528cbcb285.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
